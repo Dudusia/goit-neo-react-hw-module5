0d74191e4522b7a995de9d18e4ec1981cb0741fb
Yevdokiia Mazhak <yevdokiia.mazhak@oag.com>
2025-08-28 20:43:27 +0300

    feat: add navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,17 @@
 import css from "./Navigation.module.css";
+import { NavLink } from "react-router-dom";
 
 export default function Navigation() {
-    return (<div>Navigation</div>)
+  return (
+    <header>
+      <ul className={css["pages-list"]}>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/movies">Movies</NavLink>
+        </li>
+      </ul>
+    </header>
+  );
 }
