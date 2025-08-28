de4cc311e763a6a46917e6e3dc57fbf890114970
Yevdokiia Mazhak <yevdokiia.mazhak@oag.com>
2025-08-28 19:35:28 +0300

    chore: bootstrap

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import css from './App.module.css';
+import { useState, useEffect } from 'react';
+
+const App = () => {
+  return (
+    <div className={css['container']}>
+    </div>
+  );
+};
+
+export default App;
