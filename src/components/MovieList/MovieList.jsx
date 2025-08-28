3c1cf373118591067a27c720bcde8ce043ab3dfd
Yevdokiia Mazhak <yevdokiia.mazhak@oag.com>
2025-08-28 22:16:51 +0300

    feat: add MovieList

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,5 +1,21 @@
 import css from "./MovieList.module.css";
+import { Link, useLocation } from "react-router-dom";
 
-export default function MovieList() {
-    return (<div>MovieList</div>)
+export default function MovieList({ movies }) {
+  const location = useLocation();
+  return (
+    <ul className={css.list}>
+      {movies.map((movie) => (
+        <li key={movie.id} className={css.item}>
+          <Link
+            to={`/movies/${movie.id}`}
+            state={{ from: location }}
+            className={css.link}
+          >
+            {movie.title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
